05a983b88017dc216a15a6b94e2cb03ba4721636
Frederick Lawler <fred@fredlawl.com>
2018-11-30 19:40:29 -0800

    Update license to GPL v2

--- mackeys.c
+++ mackeys.c
@@ -4,21 +4,19 @@
 
 #include "mackeys.h"
 
-MODULE_AUTHOR("Frederick Lawler");
-MODULE_DESCRIPTION("Remaps keyboard keys to model after useful MacOS key bindings.");
-MODULE_LICENSE("MIT");
-//MODULE_SUPPORTED_DEVICE("<insert keyboard>");
-
-static int __init init_mackeys(void)
+static int __init mackeys_init(void)
 {
 	mackeys_info("Module installed");
 	return 0;
 }
+module_init(mackeys_init);
 
-static void __exit cleanup_mackeys(void)
+static void __exit mackeys_exit(void)
 {
 	mackeys_info("Module removed");
 }
+module_exit(mackeys_exit);
 
-module_init(init_mackeys);
-module_exit(cleanup_mackeys);
+MODULE_AUTHOR("Frederick Lawler <fred@fredlawl.com>");
+MODULE_DESCRIPTION("Remaps keyboard keys to model after useful MacOS key bindings.");
+MODULE_LICENSE("GPL v2");
